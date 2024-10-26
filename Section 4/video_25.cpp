a1e5b7e65930094f2f758cad89b435a44bd4869c
Douglas Ribas de Mattos <douglasmattos0@gmail.com>
2024-10-26 13:43:37 -0300

    first commit

---
+++ Section 4/video_25.cpp
@@ -0,0 +1,34 @@
+/*
+Script Name:    video_25.cpp
+Author:         Douglas Ribas de Mattos
+Date Created:   2024-10-26
+Last Modified:  2024-10-26
+Version:        1.0
+
+Description:
+    This C++ program describe the initial concepts about C++ programming.
+    How to comment C++ code
+
+Usage:
+    Compile the program in a Linux Command Line:
+    $ g++ -Wall -std=c++17 video_25.cpp -o video_26
+    $ ./video_25
+
+Requirements:
+    - N/A
+
+Change Log:
+    - 2024-10-26 - Douglas Ribas de Mattos - Initial version
+
+Notes:
+    Ensure the input file is in the specified format and located in the same directory as this source code.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World !" << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+Script Name:    main.cpp
+Author:         Douglas Ribas de Mattos
+Date Created:   2024-10-26
+Last Modified:  2024-10-26
+Version:        1.0
+
+Description:
+    This C++ program describe the initial concepts about C++ programming.
+    How to comment C++ code
+
+Usage:
+    Compile the program in a Linux Command Line:
+    $ g++ -Wall -std=c++17 main.cpp -o main
+    $ ./main
+
+Requirements:
+    - N/A
+
+Change Log:
+    - 2024-10-26 - Douglas Ribas de Mattos - Initial version
+
+Notes:
+    Ensure the input file is in the specified format and located in the same directory as this source code.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World !" << endl;
+    return 0;
+}
